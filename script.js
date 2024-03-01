e346050651b03284eb8bed98e8000272b66bd53a
Wilczyński Kryspin <wilczynski.kryspin@zs6sobieski.edu.pl>
2024-03-01 10:07:57 +0100

    pierwszy commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var input = document.getElementById("nr_koloru")
+var gl = document.getElementById("kolorgl")
+var k2 = document.getElementById("kolor2")
+var k3 = document.getElementById("kolor3")
+var k4 = document.getElementById("kolor4")
+var k5 = document.getElementById("kolor5")
+
+
+
+
+
+
+function klik(){
+    var wartosc=input.value
+
+}
